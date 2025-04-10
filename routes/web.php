aed1912afd03334fd90699657ad66926281ce69e
Muhammad Akhtar <105409960+M-Dan-Akhtar@users.noreply.github.com>
2025-04-10 17:54:59 -0400

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test-db', function () {
+  return DB::select('SELECT DATABASE()');
+});

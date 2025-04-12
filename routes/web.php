ed57eacc6838926473cf2515d08d43c130f56f83
Muhammad Akhtar <105409960+M-Dan-Akhtar@users.noreply.github.com>
2025-04-12 19:37:06 -0400

    fixed bug where show page wouldnt route to dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [JobPostingController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', [JobPostingController::class, 'index'])->middleware('auth')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
